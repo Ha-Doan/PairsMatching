b940ad9cec0b9cbf5c9b67ba83bef0056ab96b39
Ha-Doan <doanthanhha@gmail.com>
2017-11-20 12:25:35 +0100

    Define 'index' method in 'rolescontroller.rb'

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,22 @@
 class RolesController < ApplicationController
+  before_action :set_role, only: [:show, :edit, :update]
+  before_action :authenticate_user!
+
+  def index
+    @role = current_user.role
+  end
+
+  def show; end
+
+  private
+
+  def set_role
+    @role = Role.find(params[:id])
+  end
+
+  def role_params
+    params
+      .require(:role)
+      .permit(:role, :user_id)
+  end
 end
