1191623304f232a9228ea796a734bd0604e665b0
nojas01 <j.r.nede@gmail.com>
2017-11-20 14:30:34 +0100

    undo migrations and generates add role colum to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120111514) do
+ActiveRecord::Schema.define(version: 20171120132932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "roles", force: :cascade do |t|
-    t.string "role"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_roles_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,12 +28,9 @@ ActiveRecord::Schema.define(version: 20171120111514) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "role_id"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
-  add_foreign_key "roles", "users"
-  add_foreign_key "users", "roles"
 end
