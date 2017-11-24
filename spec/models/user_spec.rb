9a6dd107543eb35d3a21ce3895f6cfe925287b3d
nojas01 <j.r.nede@gmail.com>
2017-11-24 14:19:57 +0100

    fixed tests and code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create :user }
+  let!(:user1) {create :user}
+  let!(:user2) {create :user}
+  let!(:user3) {create :user}
+
+  describe '.find_match' do
+    it "should get matches" do
+    match = user.find_match
+
+    expect(match[0]).to include(user1.email)
+    end
+  end
 end
